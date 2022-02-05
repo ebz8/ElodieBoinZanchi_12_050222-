7979c6d15a82d1e695d85903f5f54ef5f8b59bb4
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-02-05 18:39:49 +0100

    config Routes and create Home & Profil pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import './App.scss'
+import { Routes, Route } from "react-router-dom"
+
+// composants page
+import Home from './pages/profil/profil'
+import Profil from './pages/profil/profil';
 
 function App() {
+
   return (
-    <div className="App">hello
-    </div>
-  );
+      <div className="App">
+        <Routes>
+          <Route exact path='/' element={<Home />}/>
+          <Route path="/user/:id" element={<Profil />}/>
+          {/* <Route path="/*" element={<Error404 />}/> */}
+        </Routes>
+      </div>
+  )
 }
 
-export default App;
+export default App
---
+++ my-app/src/pages/profil/profil.js
@@ -0,0 +1,9 @@
+import './profil.scss'
+
+export default function Profil() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
