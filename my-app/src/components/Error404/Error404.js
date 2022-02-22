5442ce5973cf5dbb225550464def369aa00c38f1
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-02-22 09:46:23 +0100

    add  & link 404 component

--- my-app/src/components/Error404/Error404.js
+++ my-app/src/components/Error404/Error404.js
@@ -1,7 +1,21 @@
+import './Error404.scss'
+
+import { Link } from "react-router-dom"
+
+import HorizontalNav from '../HorizontalNav/HorizontalNav'
+import VerticalNav from '../VerticalNav/VerticalNav'
+
 export default function Error404() {
+  // créer des messages spécifiques selon erreur d'url ou d'API
+  // log l'erreur
   return (
-    <div>
-        <p>erreur 404</p>
-    </div>
+    <>
+      <HorizontalNav />
+      <VerticalNav />
+      <div className='error404'>
+        <h2>Erreur 404</h2>
+        <Link to="/">Retourner sur la page d'accueil</Link>
+      </div>
+    </>
   )
 }
